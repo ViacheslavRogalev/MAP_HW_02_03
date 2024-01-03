8b8ecd4d03478c8199f8de89cf8d985f067a0fb4
vyahislav <khv-rvv@mail.ru>
2024-01-04 00:48:09 +1000

    first commit

---
+++ Swap.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <mutex>
+#include "Data.h"
+
+namespace own {
+
+	void swap_lock(Data& alfa, Data& betta);
+	void swap_scopedlock(Data& alfa, Data& betta);
+	void swap_uniquelock(Data& alfa, Data& betta);
+}
